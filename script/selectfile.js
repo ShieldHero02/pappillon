d6be98b1382c732298877414e16ba5623f916048
ShieldHero02 <74604355+ShieldHero02@users.noreply.github.com>
2023-06-16 11:52:03 +0300

    Delete script directory

--- script/selectfile.js
+++
@@ -1,9 +0,0 @@
-$(document).ready( function() {
-    $('#bntUpload').click(function(){
-        $("#so_file").click();
-    });
-    
-    $('#so_file').change(function() {
-        $('#selected_filename').text($('#so_file')[0].files[0].name);
-    });
-});
